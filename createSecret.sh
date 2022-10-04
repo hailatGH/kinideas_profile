8c3fdf9c26ed27721b20416b807e7327d6d1e345
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-10-04 16:42:21 +0300

    done

--- createSecret.sh
+++ createSecret.sh
@@ -1,8 +1,8 @@
-gcloud sql databases create kinmusic-kinideas-profile-database --instance=kinmusic-postgresql-v14
+gcloud sql databases create kinmusic-kinideas-profile-database-dev --instance=kinmusic-postgresql-v14
 
-gcloud secrets create kinideas_profile_settings --replication-policy automatic
-gcloud secrets versions add kinideas_profile_settings --data-file .env
+gcloud secrets create kinideas_profile_settings_dev --replication-policy automatic
+gcloud secrets versions add kinideas_profile_settings_dev --data-file .env.dev
 
-gcloud secrets add-iam-policy-binding kinideas_profile_settings \
+gcloud secrets add-iam-policy-binding kinideas_profile_settings_dev \
     --member serviceAccount:299791645258@cloudbuild.gserviceaccount.com \
     --role roles/secretmanager.secretAccessor
