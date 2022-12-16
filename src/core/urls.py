0bf921306552d50171cb91e6bef517df36570575
hailatGH <hailat.alx@gmail.com>
2022-12-16 09:47:35 +0300

    openapi set

--- src/core/urls.py
+++ src/core/urls.py
@@ -23,22 +23,25 @@ from drf_yasg import openapi
 from .routers import router
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Kinideas Profile API",
-      default_version='v1',
-      description="Kinideas Profile API description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="admin@kinideas.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="Kinideas Profile API",
+        default_version='v1',
+        description="Kinideas Profile API description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="admin@kinideas.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('OpenAPI_Config_Kinideas_Profile.yaml', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    path('openapi.yml', schema_view.without_ui(
+        cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
