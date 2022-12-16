3acd6d42668810152b443c81de184bbc3fcb1a9a
hailatGH <hailat.alx@gmail.com>
2022-12-16 15:36:58 +0300

    local

--- src/user/views.py
+++ src/user/views.py
@@ -2,19 +2,23 @@ from rest_framework import viewsets, status
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from datetime import date, datetime, timedelta
+from dateutil.relativedelta import *
 
 from user.serializers import *
 from .models import *
 
+
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
+
 
 class UserPrivilegeViewset(viewsets.ModelViewSet):
 
     queryset = UserPrivilegeModel.objects.all()
     serializer_class = UserPrivilegeSerializer
     pagination_class = StandardResultsSetPagination
+
 
 class SubscribedUsersViewSet(viewsets.ModelViewSet):
 
@@ -23,27 +27,42 @@ class SubscribedUsersViewSet(viewsets.ModelViewSet):
     pagination_class = StandardResultsSetPagination
 
     def create(self, request, *args, **kwargs):
-        request.data._mutable = True
-        if request.data['subscription_type'].lower() == "monthly":
-            request.data['subscription_expiry_date'] = datetime.now() + timedelta(days=30)
-        elif request.data['subscription_type'].lower() == "yearly":
-            request.data['subscription_expiry_date'] = datetime.now() + timedelta(days=365)
-        return super().create(request, *args, **kwargs)
+        if request.data['subscription_type'] == "MONTHLY":
+            date = datetime.now()
+            new_paid_until_monthly = date + relativedelta(months=+1)
 
-    def update(self, request, *args, **kwargs):
-        request.data._mutable = True
-        if request.data['subscription_type'].lower() == "monthly":
-            request.data['subscription_expiry_date'] = datetime.now() + timedelta(days=30)
-        elif request.data['subscription_type'].lower() == "yearly":
-            request.data['subscription_expiry_date'] = datetime.now() + timedelta(days=365)
-        return super().update(request, *args, **kwargs)
+            pay_load = {}
+            pay_load = {
+                "user_id": request.data['user_id'],
+                "subscription_type": "MONTHLY",
+                "subscription_expiry_date": new_paid_until_monthly
+
+            }
+        elif request.data['subscription_type'] == "YEARLY":
+            date = datetime.now()
+            new_paid_until_yearly = date + relativedelta(months=+12)
+
+            pay_load = {}
+            pay_load = {
+                "user_id": request.data['user_id'],
+                "subscription_type": "YEARLY",
+                "subscription_expiry_date": new_paid_until_yearly
+
+            }
+
+        serializer = self.serializer_class(
+            data=pay_load)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class NoOfSkipsViewSet(viewsets.ModelViewSet):
 
     queryset = NoOfSkipsModel.objects.all()
     serializer_class = NoOfSkipsSerializer
     pagination_class = StandardResultsSetPagination
- 
+
     def create(self, request, *args, **kwargs):
         return Response("Method not allowed")
 
@@ -53,7 +72,8 @@ class NoOfSkipsViewSet(viewsets.ModelViewSet):
         if not self.queryset.filter(user_id=userId).exists():
             return super().create(request, *args, **kwargs)
 
-        userData = self.queryset.filter(user_id=userId).values('user_id','noOfSkips','created_at','updated_at')
+        userData = self.queryset.filter(user_id=userId).values(
+            'user_id', 'noOfSkips', 'created_at', 'updated_at')
         today = str(date.today())
         lastUpdateDate = str(userData[0]['updated_at'])[:len(today)]
 
