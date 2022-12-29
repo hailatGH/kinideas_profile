f742f99e3c02f1cbe44a73e7d423f45ae68a95d0
hailatGH <hailat.alx@gmail.com>
2022-12-29 11:10:13 +0300

    feature release

--- src/user/models.py
+++ src/user/models.py
@@ -60,6 +60,3 @@ class FeatureRelease(models.Model):
         null=False, blank=False, default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.id}"
