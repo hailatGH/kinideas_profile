6d9c3af88c69a90193e14f77072e2806d50b35a2
hailatGH <hailat.alx@gmail.com>
2022-12-29 10:54:26 +0300

    feature release

--- src/core/routers.py
+++ src/core/routers.py
@@ -5,5 +5,8 @@ from user.views import *
 router = DefaultRouter(trailing_slash=False)
 
 router.register(r'users', UserPrivilegeViewset, basename="users")
-router.register(r'subscribedUsers', SubscribedUsersViewSet, basename="subscribedUsers")
+router.register(r'subscribedUsers', SubscribedUsersViewSet,
+                basename="subscribedUsers")
 router.register(r'noOfSkips', NoOfSkipsViewSet, basename="noOfSkips")
+router.register(r'featureRelease', FeatureReleaseViewSet,
+                basename="featureRelease")
